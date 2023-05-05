89d9c51b3533317a0e50dd6c5b6500d7f75e6163
MOHAMMED EL-IDRISSI <ingmedical16@gmail.com>
2023-05-05 15:53:38 +0200

    Redux Basic

---
+++ 12_redux-demo/redux-demo.js
@@ -0,0 +1,33 @@
+const redux = require('redux');
+
+const counterReducer = (state, action) => {
+
+
+    if (action.type === 'increment') {
+        return {
+            counter: state.counter + 1
+        }
+    }
+    if (action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    return state
+}
+
+// store
+const store = redux.createStore(counterReducer, {
+    counter: 0
+});
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment' });
+store.dispatch({ type: 'decrement' });
