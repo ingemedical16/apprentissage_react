d6941f11203af932cfb5997b30437e7780886e8f
MOHAMMED EL-IDRISSI <ingmedical16@gmail.com>
2023-05-11 18:43:51 +0200

    authentication

---
+++ 18_authentication/frontend/src/pages/Root.jsx
@@ -0,0 +1,38 @@
+import { Outlet, useLoaderData,useSubmit } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
+import { getTokenDuration } from '../utils/auth';
+
+function RootLayout() {
+  // const navigation = useNavigation();
+  
+  const token = useLoaderData();
+  const submit = useSubmit();
+  useEffect(()=>{
+    if(!token){
+      return;
+    }
+    if(token === 'EXPRRED' ){
+      submit(null,{action:'/logout',method:'POST'})
+    }
+
+    const tokenDuration = getTokenDuration()
+    console.log(tokenDuration);
+    setTimeout(()=>{
+     submit(null,{action:'/logout',method:'POST'})
+    },tokenDuration)
+  },[token,submit])
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
