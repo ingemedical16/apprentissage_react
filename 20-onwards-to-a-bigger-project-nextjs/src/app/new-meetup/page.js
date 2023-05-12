b78a861baa8f170b2ee05cd69f1b132b22bbdff7
MOHAMMED EL-IDRISSI <ingmedical16@gmail.com>
2023-05-12 17:39:25 +0200

    diving into NextJS

---
+++ 20-onwards-to-a-bigger-project-nextjs/src/app/new-meetup/page.js
@@ -0,0 +1,16 @@
+"use client"
+import NewMeetupForm from '@/components/meetups/NewMeetupForm';
+import React, { Fragment } from 'react';
+
+function NewMeetup(props) {
+    const addMeetupHandler = (data)=>{
+        console.log(data)
+    };
+    return (
+        <Fragment>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </Fragment>
+    );
+}
+
+export default NewMeetup;
